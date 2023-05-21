4bbd1a354108b62285a7879dfb4e9dad95680115
Paula Segatelli <pc.segatelli@gmail.com>
2023-05-20 21:03:39 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,23 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+ 
+	public static void main(String[] args) {
+		int a =2;
+		int h =3;
+		System.out.println("Hello-World! "+(a+h));
+	
+		Gato gato = new Gato();	
+		Livros livros = new Livros();	
+		
+		System.out.println(gato);
+		System.out.println(livros);
+	}
+}
+	
+	class Livros{
+		private String nome;
+		private String npag;
+}
